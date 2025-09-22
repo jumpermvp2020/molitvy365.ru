e8f58b33cb3d28849309e055d77ae5afeb617bea
ivanbuhtiarov <iskyi48@yandex.ru>
2025-09-22 19:28:38 +0700

    project start

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -5,20 +5,20 @@ import Script from 'next/script';
 const GA_TRACKING_ID = 'G-QHJE4FDXH1';
 
 export default function GoogleAnalytics() {
-  return (
-    <>
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
+    return (
+        <>
+            <Script
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+                strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', '${GA_TRACKING_ID}');
         `}
-      </Script>
-    </>
-  );
+            </Script>
+        </>
+    );
 }
